b0d38e388de86ee3f0b2973eabd71a9167c01edf
Abel Martin <abel.martin@gmail.com>
2014-03-22 01:50:40 -0400

    Added a users table and needed gems.

---
+++ db/migrate/20140322040523_add_users_table.rb
@@ -0,0 +1,11 @@
+class AddUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name, limit: 16
+      t.string :last_name, limit: 16
+      t.string :email, limit: 64
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
